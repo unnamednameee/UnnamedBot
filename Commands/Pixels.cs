40f644235336b5e14815b76470c0b775869bb793
unnamednameee <unnamedname999@gmail.com>
2025-02-01 22:30:59 +0300

    refactoring

--- Commands/Pixels.cs
+++ Commands/Pixels.cs
@@ -16,7 +16,7 @@ public class Pixels
     public static async ValueTask ExecuteSlashCommandAsync(CommandContext context, [Description("Attachment to count pixels in")] DiscordAttachment attachment)
     {
         await context.DeferResponseAsync();
-        await context.RespondAsync(GetResponse(attachment));
+        await context.RespondAsync(GetPixelsInAttachmentInfo(attachment));
     }
 
     [Command("pixels-m"), DisplayName("Count pixels"), Description("Count pixels in attachments")]
@@ -29,25 +29,33 @@ public class Pixels
         var attachments = message.Attachments;
         if (attachments.Count == 0)
         {
-            await context.RespondAsync("This message doesn't have attachments.");
+            var response = "This message doesn't have attachments.";
+            await context.RespondAsync(response);
             return;
         }
         else if (attachments.Count == 1)
         {
             var attachment = attachments[0];
-            await context.RespondAsync(GetResponse(attachment));
+            var response = GetPixelsInAttachmentInfo(attachment);
+            await context.RespondAsync(response);
             return;
         }
 
+        // if there are multiple attachments, count all of their pixels
         var responseBuilder = new StringBuilder();
         responseBuilder.AppendLine($"This message has {attachments.Count} attachments:");
         var totalPixelsCount = 0;
         for (int i = 0; i < attachments.Count; i++)
         {
-            responseBuilder.AppendLine($"{i + 1}. {GetResponse(attachments[i])}");
-            totalPixelsCount += GetPixelsCount(attachments[i]);
+            var attachment = attachments[i];
+            var pixelsInfo = GetPixelsInAttachmentInfo(attachment);
+            responseBuilder.AppendLine($"{i + 1}. {pixelsInfo}");
+
+            var pixelsCount = GetPixelsCount(attachment);
+            totalPixelsCount += pixelsCount;
         }
-        responseBuilder.AppendLine($"Total pixels: {Format(totalPixelsCount)}.");
+        var formattedTotalPixels = FormatWithCommas(totalPixelsCount);
+        responseBuilder.AppendLine($"Total pixels: {formattedTotalPixels}.");
         await context.RespondAsync(responseBuilder.ToString());
     }
 
@@ -56,11 +64,15 @@ public class Pixels
             ? (int)attachment.Width * (int)attachment.Height
             : 0;
 
-    private static string Format(int number) => number.ToString("N0", CultureInfo.GetCultureInfo("en-US"));
+    // adds commas to big numbers (12345678 -> 12,345,678)
+    private static string FormatWithCommas(int number)
+        => number.ToString("N0", CultureInfo.GetCultureInfo("en-US"));
 
-    private static string Format(int? number) => ((int)number!).ToString("N0", CultureInfo.GetCultureInfo("en-US"));
+    // same thing but for nullable integers
+    private static string FormatWithCommas(int? number)
+        => ((int)number!).ToString("N0", CultureInfo.GetCultureInfo("en-US"));
 
-    private static string GetResponse(DiscordAttachment attachment)
+    private static string GetPixelsInAttachmentInfo(DiscordAttachment attachment)
     {
         var invalidFileResponse = "This attachment is not supported.";
         var wtfResponse = "Couldn't count pixels; something went wrong.";
@@ -74,11 +86,11 @@ public class Pixels
             if (GetPixelsCount(attachment) == 0)
                 return wtfResponse;
 
-            var mediaTypeAndExtension = mediaType.Split('/'); // 0 - type, 1 - extension
+            var mediaTypeAndExtension = mediaType.Split('/'); // [0] - type, [1] - extension
             return mediaTypeAndExtension[0] switch // checking media type without extension
             {
-                "image" => $"{(mediaTypeAndExtension[1] != "gif" ? "This image has" : "A frame in this GIF has")} {FormatPixelsCountInfo(attachment)}",
-                "video" => $"A frame in this video has {FormatPixelsCountInfo(attachment)}",
+                "image" => $"{(mediaTypeAndExtension[1] != "gif" ? "This image has" : "A frame in this GIF has")} {FormatPixelsCount(attachment)}",
+                "video" => $"A frame in this video has {FormatPixelsCount(attachment)}.",
                 _ => invalidFileResponse
             };
         }
@@ -88,6 +100,13 @@ public class Pixels
         }
     }
 
-    private static string FormatPixelsCountInfo(DiscordAttachment attachment)
-        => $"{Format(GetPixelsCount(attachment))} ({Format(attachment.Width)}×{Format(attachment.Height)}) pixel{(GetPixelsCount(attachment) == 1 ? "" : "s")}.";
+    private static string FormatPixelsCount(DiscordAttachment attachment)
+    {
+        int attachmentPixelsCount = GetPixelsCount(attachment);
+        string attachmentPixels = FormatWithCommas(attachmentPixelsCount);
+        string width = FormatWithCommas(attachment.Width);
+        string height = FormatWithCommas(attachment.Height);
+        string s = attachmentPixelsCount == 1 ? "" : "s";
+        return $"{attachmentPixels} ({width}×{height}) pixel{s}";
+    }
 }
